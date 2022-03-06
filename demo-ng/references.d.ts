ed70b158a78eb1b1b46c9daac951dfad4c81fcde
Martin Guillon <martin.guillon@akylas.fr>
2022-03-06 15:12:24 +0100

    chore: cleanup

--- demo-ng/references.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/lib/android-20.d.ts" />
--- demo-ng/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
-
-import { AppModule } from './app/app.module';
-
-runNativeScriptAngularApp({
-	appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
-});
